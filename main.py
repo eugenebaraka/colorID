cd109dc8c85d2baa76304d59abafc0109afdb23a
Eugene Baraka <97700750+eugenebaraka@users.noreply.github.com>
2023-08-21 17:38:34 -0400

    project outline

---
+++ main.py
@@ -0,0 +1,11 @@
+# Project outline
+
+# 1. GUI design (use PyQt or Tkinter)
+# 2. capture mouse hover
+# 3. Image processing to pick get color information (look into OpenCV)
+# 4. Converting color to hex code and RGB values
+# 5. Display results in the GUI area
+
+# main features
+## automatically copy on clipboard
+## history of colors picked
